83ce4805d0995b9537e4f73710e2c3fa4af5a3d9
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-07-18 05:16:43 -0500

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,10 +8,13 @@ CREATE TABLE invoices_items (id SERIAL PRIMARY KEY NOT NULL, unit_price DECIMAL(
 FOREIGN KEY (invoice_id) REFERENCES invoices(id),
 FOREIGN KEY (treatment__id) REFERENCES treatments(id)
 );
-CREATE TABLE medical_histories_treatments (id SERIAL PRIMARY KEY NOT NULL, medical_history__id int references medical_histories(id),  treatments__id int references treatments(id),status VARCHAR(100) );
+CREATE TABLE medical_histories_treatments (id SERIAL PRIMARY KEY NOT NULL, FOREIGN KEY medical_history__id int references medical_histories(id), FOREIGN KEY treatments__id int references treatments(id), status VARCHAR(100) );
 
 
 
 CREATE INDEX idx_invoices ON invoices (medical_history__id);
+CREATE INDEX idx_medical_histories ON medical_histories (patient_id);
+CREATE INDEX idx_invoices_items_inv ON invoices_items (invoice_id);
+CREATE INDEX idx_invoices_items_treatment ON invoices_items (treatment__id);
 CREATE INDEX idx_medical_histories_treatments ON medical_histories_treatments (medical_history__id);
 CREATE INDEX idx_medical_histories_treatments_two ON medical_histories_treatments (treatments__id);
